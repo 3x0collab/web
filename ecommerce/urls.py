af27aa538c53797c606b4f85a1f74908cf1e8bab
3x0 <128635963+3x0collab@users.noreply.github.com>
2024-08-12 11:18:42 +0100

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -29,5 +29,4 @@ path('admin/', admin.site.urls),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
-]
-urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
